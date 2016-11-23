b631141c8791e9ae2dc9f5024f9f5ed477c7391a
Viktor René <viktor.rene@gmail.com>
2016-11-23 23:27:35 +0100

    added analytics

--- deploy/index.php
+++ deploy/index.php
@@ -28,6 +28,15 @@
 	<link rel="stylesheet" type="text/css" href="/css/master.css"/>
 	<script type="text/javascript" src="/js/libraries.js"></script>
 	<script type="text/javascript" src="/js/script.js"></script>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-45283243-3', 'auto');
+		ga('send', 'pageview');
+	</script>
 </head>
 <body>
 	<h1>EmulationStation es_systems.cfg generator</h1>
