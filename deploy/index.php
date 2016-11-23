0a5865651c28c2629b4337e15c183d4ea5eb5402
Viktor René <viktor.rene@gmail.com>
2016-11-23 22:51:41 +0100

    initial commit

---
+++ deploy/index.php
@@ -0,0 +1,66 @@
+<?php 
+	$title = "Emulationstation Systems Generator";
+	$description = "A tool for generating the es_systems.cfg file for EmulationStation";
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?=$title;?></title>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, target-densitydpi=device-dpi, maximum-scale=1, minimum-scale=1"/>
+
+	<meta property="og:title" content="<?=$title;?>" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="" />
+	<meta property="og:image:type" content="image/png" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="600" />
+	<meta property="og:description" content="<?=$description;?>" />
+
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@_tEvEy">
+	<meta name="twitter:title" content="<?=$title;?>">
+	<meta name="twitter:description" content="<?=$description;?>">
+	<meta name="twitter:image" content="">
+
+	<meta name="application-name" content="<?=$title;?>"/>
+	<meta name="description" content="<?=$description;?>" />
+	<meta name="author" content="tevey (@_tEvEy)">
+	<meta name="keywords" content="EmulationStation, rom, emulator"/>
+
+	<link rel="stylesheet" type="text/css" href="/css/reset.css"/>
+	<link rel="stylesheet" type="text/css" href="/css/master.css"/>
+	<script type="text/javascript" src="/js/libraries.js"></script>
+	<script type="text/javascript" src="/js/script.js"></script>
+</head>
+<body>
+	<h1>EmulationStation es_systems.cfg generator</h1>
+	<p><span class="code">es_systems.cfg</span> is the main configuration file for EmulationStation. It is located at <span class="code">~/.emulationstation/es_systems.cfg</span> . It is an XML document that defines a list of systems .</p>
+	<form id="system-form">
+		<select class="system-select">
+			<option value="none">Select System</option>
+		</select>
+		<br>
+		<input type="text" id="path" placeholder="Path to roms" required>
+		<p class="help-button path-help-button">?</p><br>
+		<p class="path-help help-content">The path to the top-most folder to start searching for games in. If the first character is '~', it will be expanded to the home folder. All subdirectories will be included. You can use forward slashes for any platform and backslashes on Windows.</p>
+		<input type="text" id="command" placeholder="Launch Command" required>
+		<p class="help-button command-help-button">?</p>
+		<div class="command-help help-content">
+			<p>Launch commands depend on what emulator you want to use. 95% of the time, <span class="code">/path/to/emulator %ROM%</span> or <span class="code">C:\path\to\emulator.exe "%ROM_RAW%"</span> works. If you're using MAME, <span class="code">/path/to/mame %BASENAME%</span> should do the trick. The following strings are replaced in your launch command:</p>
+			<br>
+			<ul>
+				<li><span class="code">%ROM%</span> - Replaced with the shell-escaped absolute path to the selected ROM. On Linux, most Bash special characters are escaped with a backslash. On Windows, the ROM path is automatically enclosed in quotes, which is equivalent to "%ROM_RAW%" (as of version 2.0.1).</li>
+				<li><span class="code">%BASENAME%</span> - Replaced with the "base" name of the path to the selected ROM. For example, a path of "/foo/bar.rom", this tag would be "bar". This tag is useful for setting up MAME.</li>
+				<li><span class="code">%ROM_RAW%</span> - Replaced with the unescaped, absolute path to the selected ROM. If your emulator is picky about paths or you are on Windows, you might want to use this instead of %ROM%, but enclosed in quotes. For example: higan "%ROM_RAW%" .</li>
+			</ul>
+		</div><br>
+		<button class="add-button">Add System</button>
+	</form>
+	<h3>Systems added:</h3>
+	<ul class="added-systems">
+	</ul>
+	<button class="download-button">Download es_systems.cfg</button>
+	<p>Get more info about EmulationStation and download at <a href="http://emulationstation.org" target="_blank">emulationstation.org</a></p>
+</body>
+</html>
---
+++ deploy/js/script.js
@@ -0,0 +1,99 @@
+var systems;
+var addedSystems = [];
+var selectedSystem;
+var XML;
+
+$(function(){
+	loadData();
+
+	$('.path-help-button').click(function(event) {
+		$('.path-help').slideToggle();
+	});
+
+	$('.command-help-button').click(function(event) {
+		$('.command-help').slideToggle();
+	});
+
+	$('.system-select').change(function(event) {
+		if($(this).val() == 'none')
+			return;
+
+		selectedSystem = getSystemByName($(this).val());
+		$('#path').attr('placeholder', 'Path to ' + selectedSystem.name + ' roms');
+	});
+
+	$('#system-form').submit(function(event) {
+		event.preventDefault();
+		if($('.system-select').val() == 'none')
+			return;
+		selectedSystem.path = $('#path').val();
+		selectedSystem.command = $('#command').val();
+		$('#path').val('');
+		$('#command').val('');
+		addSystem(selectedSystem);
+	});
+
+	$('.download-button').click(function(event) {
+		generateXML();
+	});
+});
+
+function loadData() {
+	$.getJSON('data.json', function(json, textStatus) {
+		systems = json.systems;
+
+		for (var i = 0; i < systems.length; i++) {
+			$('.system-select').append('<option value="' + systems[i].name + '">' + systems[i].fullname + '</option>')
+		}
+	});
+}
+
+function getSystemByName(name) {
+	for (var i = 0; i < systems.length; i++) {
+		if(systems[i].name == name) {
+			return systems[i];
+		}
+	}
+}
+
+function isDuplicate(name) {
+	for (var i = 0; i < addedSystems.length; i++) {
+		if(addedSystems[i].name == name) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function addSystem(system) {
+
+	$('.download-button').css('display', 'block');
+
+	if(!isDuplicate(system.name)) {
+		addedSystems.push(system);
+	}
+	$('.added-systems').children().remove();
+	for (var i = 0; i < addedSystems.length; i++) {
+		$('.added-systems').append('<li>' + addedSystems[i].fullname + '</li>')
+	}
+}
+
+function generateXML() {
+	XML = '<systemList>\n';
+	for (var i = 0; i < addedSystems.length; i++) {
+		var system = addedSystems[i];
+		XML += '\t<system>\n';
+		XML += '\t\t<name>' + system.name + '</name>\n';
+		XML += '\t\t<fullname>' + system.fullname + '</fullname>\n';
+		XML += '\t\t<path>' + system.path + '</path>\n';
+		XML += '\t\t<extension>' + system.extension + '</extension>\n';
+		XML += '\t\t<command>' + system.command + '</command>\n';
+		XML += '\t\t<platform>' + system.platform + '</platform>\n';
+		XML += '\t\t<theme>' + system.theme + '</theme>\n';
+		XML += '\t</system>\n';
+	}
+	XML += '</systemList>';
+
+	var blob = new Blob([XML], {type: 'text/plain;charset=utf-8'});
+	saveAs(blob, 'es_systems.cfg');
+}
